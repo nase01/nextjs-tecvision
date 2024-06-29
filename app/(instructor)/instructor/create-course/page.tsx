f42b044453efd0f2de033c411d518558c6b1a88c
Sony <nasesony01@gmail.com>
2024-06-29 16:30:03 +0800

    Implement CreateCourseForm

--- app/(instructor)/instructor/create-course/page.tsx
+++ app/(instructor)/instructor/create-course/page.tsx
@@ -1,6 +1,27 @@
-const CreateCoursePage = () => {
+import CreateCourseForm from "@/components/courses/CreateCourseForm"
+import { db } from "@/lib/db"
+
+const CreateCoursePage = async () => {
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc"
+    },
+    include: {
+      subCategories: true
+    }
+  })
+
   return (
-    <div>page</div>
+    <div>
+      <CreateCourseForm categories={categories.map((category) => ({
+        label: category.name,
+        value: category.id,
+        subCategories: category.subCategories.map((subcategory) => ({
+          label: subcategory.name,
+          value: subcategory.id
+        }))
+      }))} />
+    </div>
   )
 }
 
