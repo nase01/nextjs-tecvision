bf6210f65eff00e8ceec6058591b79d47b1682ba
Sony <nasesony01@gmail.com>
2024-07-28 21:15:02 +0800

    Implement course section details

---
+++ components/sections/SectionsDetails.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import {
+  Course,
+  MuxData,
+  Progress,
+  Purchase,
+  Resource,
+  Section,
+} from "@prisma/client";
+import { useState } from "react";
+import { File, Lock } from "lucide-react";
+
+import ReadText from "@/components/custom/ReadText";
+import MuxPlayer from "@mux/mux-player-react";
+import Link from "next/link";
+import SectionMenu from "../layout/SectionMenu";
+
+interface SectionsDetailsProps {
+  course: Course & { sections: Section[] };
+  section: Section;
+  purchase: Purchase | null;
+  muxData: MuxData | null;
+  resources: Resource[] | [];
+  progress: Progress | null;
+}
+
+const SectionsDetails = ({
+  section,
+  purchase,
+  muxData,
+  resources,
+}: SectionsDetailsProps) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const isLocked = !purchase && !section.isFree;
+
+  return (
+    <div className="px-6 py-4 flex flex-col gap-5">
+      <div className="flex flex-col md:flex-row md:justify-between md:items-center">
+        <h1 className="text-2xl font-bold max-md:mb-4">{section.title}</h1>
+
+        {
+          // Buy Button and Progress
+        }
+      </div>
+
+      <ReadText value={section.description!} />
+
+      {isLocked ? (
+        <div className="px-10 flex flex-col gap-5 items-center bg-[#FFF8EB]">
+          <Lock className="h-8 w-8" />
+          <p className="text-sm font-bold">
+            Video for this section is locked!. Please buy the course to access
+          </p>
+        </div>
+      ) : (
+        <MuxPlayer
+          playbackId={muxData?.playbackId || ""}
+          className="md:max-w-[600px]"
+        />
+      )}
+
+      <div>
+        <h2 className="text-xl font-bold mb-5">Resources</h2>
+        {resources.map((resource) => (
+          <Link
+            key={resource.id}
+            href={resource.fileUrl}
+            target="_blank"
+            className="flex items-center bg-[#FFF8EB] rounded-lg text-sm font-medium p-3"
+          >
+            <File className="h-4 w-4 mr-4" />
+            {resource.name}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SectionsDetails;
